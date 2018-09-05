9509ea95ac764f7634ad3da4bcd79d0de8bd4398
Vitaly Litvak <vitavaque@gmail.com>
2018-09-05 22:32:05 +0300

    Implemented Docker Container for MySQL

--- mysql/start.sh
+++ mysql/start.sh
@@ -1,5 +1,3 @@
-echo $MYSQL_HOST
-
 echo "Waiting for mysql"
 until mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &> /dev/null
 do
@@ -7,12 +5,13 @@ do
   sleep 1
 done
 
-if [ -e `mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW TABLES" $MYSQL_DATABASE` ]; then
+if [ `mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW TABLES" $MYSQL_DATABASE | wc -l` = 0 ]; then
 	echo "-- Creating database"
 	mysql -h"$MYSQL_HOST" -P"$MYSQL_PORT" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" $MYSQL_DATABASE < schema.sql
 fi
 
-sed -i "s/\$\[DBHOST\]/$MYSQL_HOST\:$MYSQL_PORT/g" conf/traccar.xml
+sed -i "s/\$\[DBHOST\]/$MYSQL_HOST/g" conf/traccar.xml
+sed -i "s/\$\[DBPORT\]/$MYSQL_PORT/g" conf/traccar.xml
 sed -i "s/\$\[DBNAME\]/$MYSQL_DATABASE/g" conf/traccar.xml
 sed -i "s/\$\[DBUSER\]/$MYSQL_USER/g" conf/traccar.xml
 sed -i "s/\$\[DBPASSWORD\]/$MYSQL_PASSWORD/g" conf/traccar.xml
